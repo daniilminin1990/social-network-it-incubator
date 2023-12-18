1d0fe86871b53fbc1ba2a57b2fa80e15fafef179
Daniil <daniilminin1990@yandex.ru>
2023-12-18 18:26:51 +0600

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://e7.pngegg.com/pngimages/92/187/png-clipart-logo-company-business-creative-company-logo-free-logo-design-template-company.png"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
